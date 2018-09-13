35b9e253017d543780215c425f82c772fbe379c2
Philip Fowler <philipwfowler@gmail.com>
2018-09-13 13:38:58 +0100

    reverting

--- bin/snpit-run.py
+++ bin/snpit-run.py
@@ -14,10 +14,10 @@ import argparse
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--vcf",required=False,help="the path to the VCF file to read and classify (can be zip/gzipped)",dest='vcf')
-    parser.add_argument("--fasta",required=False,help="the path to the fasta file (gzipped)",dest='fasta')
+    parser.add_argument("--input",required=True,help="the path to the VCF file to read and classify (can be zip/gzipped)")
     options = parser.parse_args()
 
+<<<<<<< HEAD
     fasta = options.fasta
     vcf= options.vcf
 
@@ -26,28 +26,26 @@ if __name__ == "__main__":
     else:
         input =vcf
     
+=======
+>>>>>>> parent of f063497... reverting to version 20d31b5 due to bugs
     # now try opening the specified input file
     try:
-        TEST = open(input,'r')
+        TEST = open(options.input,'r')
     except IOError:
-        print("input file "+input+" does not exist!")
+        print("input file "+options.input+" does not exist!")
 
 
     # create an instance (this loads all the lineages)
     tb_lineage_collection=snpit(threshold=10)
 
     # load the VCF file into the SNPIT instance (this automatically resets the list of SNPs for each lineage)
-    if vcf:
-    	tb_lineage_collection.load_vcf(input.rstrip())
-    elif fasta:
-    	tb_lineage_collection.load_fasta(input.rstrip())
-    else:
-        sys.stdout.write('Please provide input files')
+    tb_lineage_collection.load_vcf(options.input.rstrip())
+
     # determine the most likely lineage/sub-lineage using snpit
     (lineage,percentage)=tb_lineage_collection.determine_lineage()
 
     # print("Most likely lineage is ...")
     if percentage is not None:
-        	print("%40s %16s %.1f %%" % (input,lineage,percentage))
+        print("%40s %16s %.1f %%" % (options.input,lineage,percentage))
     else:
-        print("%40s %16s %.1f %%" % (input,"none identified",0))
+        print("%40s %16s %.1f %%" % (options.input,"none identified",0))
