19f35ad9bf0b036fdff47278a620253001c2ddc0
Michael Hall <mbhall88@gmail.com>
2019-06-21 17:09:34 +0100

    update setup

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 from pathlib import Path
 
 long_description = Path(__file__).parent.joinpath("README.md").read_text()
@@ -12,18 +12,24 @@ short_description = (
 package_data = list(Path(__file__).parent.joinpath("lib").rglob("*"))
 package_data.append("*.md")
 
+setup_requirements = ["pysam"]
+
+test_requirements = ["pytest", "pysam"]
+
 setup(
     name="snpit",
     version="1.0.0",
     author="Samuel Lipworth",
-    packages=["snpit"],
+    packages=find_packages(include=["snpit"]),
     package_data={"snpit": package_data},
     include_package_data=True,
-    install_requires=["pysam"],
-    test_requirements=["pytest"],
     license="MIT",
     short_description=short_description,
     long_description=long_description,
     url="https://github.com/philipwfowler/snpit",
     entry_points={"console_scripts": ["snpit = snpit.__main__:main"]},
+    setup_requires=setup_requirements,
+    test_suite="tests",
+    tests_require=test_requirements,
+    zip_safe=False,
 )
